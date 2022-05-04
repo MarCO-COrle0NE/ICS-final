5e19f0816e36c61b2277ea41655aa7b274f4bd94
Lihanyiyi <lf2383@nyu.edu>
2022-05-04 22:09:12 +0800

    add tic tac toe1.py (update some fuctions,remain last circumstance)

--- chat_system_gui/tic-tac-toe1.py
+++ chat_system_gui/tic-tac-toe1.py
@@ -2,12 +2,64 @@ import tkinter
 from tkinter import *
 from tkinter import messagebox
 
-
+class Game:
+    def __init__(self):
+        self.root=Tk()
+        self.root.title("Tic Tac Toe 2.0")
+        self.root.geometry("450x700")
+        self.clicked=True
+        self.count=0 #可能要改成oop
 root=Tk()
 root.title("Tic Tac Toe 2.0")
 root.geometry("450x700")
 clicked=True
 count=0
+# dict to record the remaining chess pieces
+dict_x={"x":3,"xx":3,"xxx":3}
+dict_o={"o":3,"oo":3,"ooo":3}
+# Button里没有text时，选完piece之后的操作
+def root1_b_small_click(b):
+    global clicked,winner,count
+    if dict_x["x"]==0:
+        messagebox.showwarning("You can't choose this button!","You don't have this piece!\nChoose another one!")
+    else:
+        b["text"] = "x"
+        dict_x["x"] -= 1
+        clicked = False
+        count += 1
+        check_if_win()
+        if winner:
+            messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+def root1_b_medium_click(b):
+    global clicked,winner,count
+    if dict_x["xx"]==0:
+        messagebox.showwarning("You can't choose this button!","You don't have this piece!\nChoose another one!")
+    else:
+        b["text"] = "xxx"
+        dict_x["xxx"] -= 1
+        clicked = False
+        count += 1
+        check_if_win()
+        if winner:
+            messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+def root1_b_big_click(b):
+    global clicked,winner,count
+    if dict_x["xxx"]==0:
+        messagebox.showwarning("You can't choose this button!","You don't have this piece!\nChoose another one!")
+    else:
+        b["text"] = "xxx"
+        dict_x["xxx"] -= 1
+        clicked = False
+        count += 1
+        check_if_win()
+        if winner:
+            messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+
+
+
+
+
+
 # Disable all buttons when game ends
 def disable_all_buttons():
     b1.config(state=DISABLED)
@@ -22,7 +74,7 @@ def disable_all_buttons():
 
 #check if someone win
 def check_if_win():
-    global winner
+    global winner,count
     winner=False
     index_x_b1,index_x_b2,index_x_b3=b1["text"].find("x"),b2["text"].find("x"),b3["text"].find("x")
     index_x_b4, index_x_b5, index_x_b6 = b4["text"].find("x"), b5["text"].find("x"), b6["text"].find("x")
@@ -36,6 +88,7 @@ def check_if_win():
         b3.config(bg="green")
         winner=True
         count+=1
+        disable_all_buttons()
         # may show win message
     elif index_o_b1!=-1 and index_o_b2!=-1 and index_o_b3!=-1:
         b1.config(bg="green")
@@ -148,22 +201,170 @@ def check_if_win():
         disable_all_buttons()
 
 
+
 # when click, what should the program do
+# x start first
 def button_click(b):
-    global clicked, count
-    pass
+    global clicked, count,dict_o,dict_x
+    # 已有最大的棋但还是点了
+    if clicked==True and b["text"]=="ooo":
+        messagebox.showwarning("You can't put your piece here!","You can't put any piece here\nbecause \'ooo\' is the largest piece!\nPlease choose another place!")
+        b.config(state=DISABLED)
+    elif clicked==False and b["text"]=="xxx":
+        messagebox.showwarning("You can't put your piece here!",\
+                               "You can't put any piece here\nbecause \'xxx\' is the largest piece!\n\
+                               Please choose another place!")
+        b.config(state=DISABLED)
+    # 有o或x在button
+    elif clicked==True and len(b["text"])==1:
+        if dict_x["xx"]==0 and dict_x["xxx"]!=0:
+            messagebox.showinfo("You only have one choice!","The big piece will be set here.")
+            b["text"]="xxx"
+            dict_x["xxx"]-=1
+            clicked=False
+            count+=1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+        elif dict_x["xxx"]==0 and dict_x["xx"]!=0:
+            messagebox.showinfo("You only have one choice!", "The medium piece will be set here.")
+            b["text"] = "xx"
+            dict_x["xx"] -= 1
+            clicked = False
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+        elif dict_x["xxx"]==0 and dict_x["xx"]==0:
+            messagebox.showwarning("You have no choice!", "You can't put any piece here\n\
+                                                       because you don't have suitable piece!\n\
+                                                       Please choose another place!")
+        else:
+            result=messagebox.askokcancel("which piece would you choose?","If you choose big piece, please click \'OK\' Button.\"\
+                                                               \n If you choose medium piece, please click \'Cancel\' Button.")
+            if result:
+                b["text"]="xxx"
+                dict_x["xxx"]-=1
+            else:
+                b["text"]="xx"
+                dict_x["xx"]-=1
+            clicked=False
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!","Congratulation! x wins!")# 可能要把x变成player的名字
+    elif clicked==False and len(b["text"])==1:
+        if dict_o["oo"]==0 and dict_o["ooo"]!=0:
+            messagebox.showinfo("You only have one choice!","The big piece will be set here.")
+            b["text"]="ooo"
+            dict_o["ooo"]-=1
+            clicked=True
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! o wins!")  # 可能要把o变成player的名字
+        elif dict_o["ooo"]==0 and dict_o["oo"]!=0:
+            messagebox.showinfo("You only have one choice!", "The medium piece will be set here.")
+            b["text"] = "oo"
+            dict_o["oo"] -= 1
+            clicked = True
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! o wins!")  # 可能要把o变成player的名字
+        elif dict_o["ooo"]==0 and dict_o["oo"]==0:
+            messagebox.showwarning("You have no choice!", "You can't put any piece here\n\
+                                                       because you don't have suitable piece!\n\
+                                                       Please choose another place!")
+        else:
+            result = messagebox.askokcancel("which piece would you choose?", "If you choose big piece, please click \'OK\' Button.\"\
+                                                                       \n If you choose medium piece, please click \'Cancel\' Button.")
+            if result:
+                b["text"]="ooo"
+                dict_o["ooo"]-=1
+            else:
+                b["text"]="oo"
+                dict_o["oo"]-=1
+            clicked=True
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! o wins!") #换player名字
+    # 有 “oo”和“xx”在button
+    elif clicked==True and len(b["text"])==2:
+        if dict_x["xxx"]!=0:
+            messagebox.showinfo("You only have one choice!","The big piece will be set here.")
+            b["text"]="xxx"
+            dict_x["xxx"]-=1
+            clicked=False
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! x wins!")  # 可能要把x变成player的名字
+        elif dict_x["xxx"]==0:
+            messagebox.showwarning("You have no choice!", "You can't put any piece here\n\
+                                                                   because you don't have suitable piece!\n\
+                                                                   Please choose another place!")
+    elif clicked==False and len(b["text"])==2:
+        if dict_o["ooo"]!=0:
+            messagebox.showinfo("You only have one choice!","The big piece will be set here.")
+            b["text"]="ooo"
+            dict_o["ooo"]-=1
+            clicked=True
+            count += 1
+            check_if_win()
+            if winner:
+                messagebox.showinfo("Game over!", "Congratulation! o wins!")  # 可能要把o变成player的名字
+        elif dict_o["ooo"]==0:
+            messagebox.showwarning("You have no choice!", "You can't put any piece here\n\
+                                                                   because you don't have suitable piece!\n\
+                                                                   Please choose another place!")
+    # Button里是空的
+    elif clicked==True and b["text"]=="":
+        root1=Tk()
+        root1.title("Choose one piece!")
+        root1_name_label = tkinter.Label(root1, text="Click the corresponding Button of the piece you want to choose!")
+        root1_name_label1=tkinter.Label(root1, text="Please close the window after the selected piece is shown!")
+        root1_name_label.grid(row=0,column=1)
+        root1_name_label1.grid(row=1,column=1)
+
+        b_small=Button(root1,text="x",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=root1_b_small_click(b))
+        b_medium=Button(root1,text="xx",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : root1_b_medium_click(b))
+        b_big=Button(root1,text="xxx",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : root1_b_big_click(b))
+
+        b_small.grid(row=2, column=0)
+        b_medium.grid(row=2, column=1)
+        b_big.grid(row=2, column=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 name_label=tkinter.Label(root, text="Let's play the game!")
 name_label.grid(row=0,column=2)
-b1=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b1))
-b2=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b2))
-b3=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b3))
-b4=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b4))
-b5=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b5))
-b6=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b6))
-b7=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b7))
-b8=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b8))
-b9=Button(root,text=" ",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b9))
+b1=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b1))
+b2=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b2))
+b3=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b3))
+b4=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b4))
+b5=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b5))
+b6=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b6))
+b7=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b7))
+b8=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b8))
+b9=Button(root,text="",font=("Times",20),height=3,width=6,bg="SystemButtonFace",command=lambda : button_click(b9))
 
 b1.grid(row=1,column=1)
 b2.grid(row=1,column=2)
