15fd52d61d93a15dc8c034a1285059bfafacae67
Lihanyiyi <99228366+Lihanyiyi@users.noreply.github.com>
2022-05-06 16:03:05 +0800

    Add files via upload

--- chat_system_gui/chat_server.py
+++ chat_system_gui/chat_server.py
@@ -180,6 +180,26 @@ class Server:
 # ==============================================================================
 # the "from" guy has had enough (talking to "to")!
 # ==============================================================================
+            elif msg["action"]=="game_connect":
+                to_name = msg["target"]
+                from_name = self.logged_sock2name[from_sock]
+                if to_name == from_name:
+                    msg = json.dumps({"action": "game_connect", "status": "self"})
+                # connect to the peer
+                elif self.group.is_member(to_name):
+                    to_sock = self.logged_name2sock[to_name]
+                    self.group.connect(from_name, to_name)
+                    the_guys = self.group.list_me(from_name)
+                    msg = json.dumps(
+                        {"action": "game_connect", "status": "success"})
+                    for g in the_guys[1:]:
+                        to_sock = self.logged_name2sock[g]
+                        mysend(to_sock, json.dumps(
+                            {"action": "game_connect", "status": "request", "from": from_name}))
+                else:
+                    msg = json.dumps(
+                        {"action": "game_connect", "status": "no-user"})
+                mysend(from_sock, msg)
             elif msg["action"] == "disconnect":
                 from_name = self.logged_sock2name[from_sock]
                 the_guys = self.group.list_me(from_name)
@@ -226,3 +246,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# update at 2022/5/6
